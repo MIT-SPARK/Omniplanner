c39a52ea4667e24ce0d23a7b6091c75ced61e071
Aaron Ray <aaronray@mit.edu>
2025-08-25 19:15:21 +0000

    hacky path publishing code

--- omniplanner_ros/src/omniplanner_ros/goto_points_ros.py
+++ omniplanner_ros/src/omniplanner_ros/goto_points_ros.py
@@ -6,23 +6,46 @@ from dataclasses import dataclass
 import numpy as np
 import omniplanner.compile_plan  # NOQA
 import spark_config as sc
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
 from omniplanner.goto_points import GotoPointsDomain, GotoPointsGoal, GotoPointsPlan
 from omniplanner.omniplanner import PlanRequest
 from omniplanner_msgs.msg import GotoPointsGoalMsg
 from plum import dispatch
-from robot_executor_interface.action_descriptions import ActionSequence, Follow
+from rclpy.clock import Clock
+from robot_executor_interface_ros.action_descriptions_ros import to_msg
+
+
+@to_msg.register
+def _(action: Path):
+    return action
 
 
 def compile_points_plan(plan: GotoPointsPlan, plan_id, robot_name, frame_id):
-    actions = []
+    path_msg = Path()
+    path_msg.header.frame_id = frame_id
+    path_msg.header.stamp = Clock().now().to_msg()
+
     for p in plan.plan:
         xs = np.interp(np.linspace(0, 1, 10), [0, 1], [p.start[0], p.goal[0]])
         ys = np.interp(np.linspace(0, 1, 10), [0, 1], [p.start[1], p.goal[1]])
-        p_interp = np.vstack([xs, ys])
-        actions.append(Follow(frame=frame_id, path2d=p_interp.T))
 
-    seq = ActionSequence(plan_id=plan_id, robot_name=robot_name, actions=actions)
-    return seq
+        for x, y in zip(xs, ys):
+            pose = PoseStamped()
+            pose.header.frame_id = frame_id
+            pose.header.stamp = (
+                Clock().now().to_msg()
+            )  # Same timestamp is okay, or could vary
+            pose.pose.position.x = x
+            pose.pose.position.y = y
+            pose.pose.position.z = 0.0
+
+            # Assuming no orientation is needed or use a default quaternion
+            pose.pose.orientation.w = 1.0  # Neutral rotation
+
+            path_msg.poses.append(pose)
+
+    return path_msg
 
 
 @dispatch
